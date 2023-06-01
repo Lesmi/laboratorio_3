2a4389ecde69dfb6d8a5a0fe9ac1d0bb6e801f8a
Uroe19 <josuenavarro95@gmail.com>
2023-05-31 19:11:38 -0600

    Toma foto

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      console.log(photo);
+      // Aquí puedes hacer algo con la foto tomada, como guardarla o mostrarla en la pantalla.
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>No tienes acceso a la cámara.</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera
+        ref={(ref) => setCamera(ref)}
+        style={styles.camera}
+        type={Camera.Constants.Type.back}
+      />
+      <TouchableOpacity style={styles.button} onPress={takePicture}>
+        <Text style={styles.buttonText}>Tomar foto</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +46,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  camera: {
+    flex: 1,
+  },
+  button: {
+    alignSelf: 'center',
+    position: 'absolute',
+    bottom: 20,
+    backgroundColor: 'blue',
+    borderRadius: 20,
+    padding: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
   },
 });
