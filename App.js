5eb89bd6ebc7ed78900ce907ed401ebf926ff6c8
Uroe19 <josuenavarro95@gmail.com>
2023-06-01 21:20:54 -0600

    Creacion de carpetas

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
   const [showPhoto, setShowPhoto] = useState(false);
+  const [folderPath, setFolderPath] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -15,18 +17,77 @@ export default function App() {
     })();
   }, []);
 
+  useEffect(() => {
+    (async () => {
+      const directory = await getPhotoDirectory();
+      setFolderPath(directory);
+    })();
+  }, []);
+
   const takePicture = async () => {
     if (camera) {
       const photoData = await camera.takePictureAsync();
       console.log(photoData);
-      setPhoto(photoData.uri);
+      const photoUri = await savePhoto(photoData.uri);
+      setPhoto(photoUri);
       setShowPhoto(true);
     }
+  };
+
+  const savePhoto = async (photoUri) => {
+    const { year, month, day } = getDateInfo();
+
+    const directory = `${FileSystem.documentDirectory}${year}`;
+    const subDirectory = `${directory}/${month}`;
+    const subSubDirectory = `${subDirectory}/${day}`;
+
+    await FileSystem.makeDirectoryAsync(directory, { intermediates: true });
+    await FileSystem.makeDirectoryAsync(subDirectory, { intermediates: true });
+    await FileSystem.makeDirectoryAsync(subSubDirectory, { intermediates: true });
+
+    const fileName = `${generateUniqueId()}.jpg`;
+    const newPhotoUri = `${subSubDirectory}/${fileName}`;
+
+    await FileSystem.moveAsync({
+      from: photoUri,
+      to: newPhotoUri,
+    });
+
+    return newPhotoUri;
+  };
+
+  const getPhotoDirectory = async () => {
+    const { year, month, day } = getDateInfo();
+
+    const directory = `${FileSystem.documentDirectory}${year}`;
+    const subDirectory = `${directory}/${month}`;
+    const subSubDirectory = `${subDirectory}/${day}`;
+
+    return subSubDirectory;
+  };
+
+  const getDateInfo = () => {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear().toString();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+
+    return { year, month, day };
+  };
+
+  const generateUniqueId = () => {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
   };
 
   const closePhoto = () => {
     setShowPhoto(false);
     setPhoto(null);
+  };
+
+  const showFolderPath = () => {
+    if (folderPath) {
+      Alert.alert('Carpeta de fotos', folderPath);
+    }
   };
 
   if (hasPermission === null) {
@@ -50,6 +111,9 @@ export default function App() {
         <Camera style={styles.camera} ref={(ref) => setCamera(ref)}>
           <TouchableOpacity style={styles.button} onPress={takePicture}>
             <Text style={styles.buttonText}>Tomar foto</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.folderButton} onPress={showFolderPath}>
+            <Text style={styles.folderButtonText}>Mostrar Carpeta</Text>
           </TouchableOpacity>
         </Camera>
       )}
@@ -79,6 +143,18 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'white',
     fontSize: 20,
+  },
+  folderButton: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    backgroundColor: 'blue',
+    borderRadius: 20,
+    padding: 10,
+  },
+  folderButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
   photoContainer: {
     flex: 1,
